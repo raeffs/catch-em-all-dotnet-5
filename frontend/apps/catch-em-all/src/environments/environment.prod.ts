6f9ab8c6c23a886a0c9026a4eb5cd442810677c1
Raphael Fleischlin <git@raeffs.dev>
2020-12-27 20:45:49 +0100

    client id

--- frontend/apps/catch-em-all/src/environments/environment.prod.ts
+++ frontend/apps/catch-em-all/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authIssuer: 'https://raefftec.eu.auth0.com/',
-  authClientId: 'w6MvGl002H07JDH86c8DzL6g46sSaeLT',
+  authIssuer: 'https://raeffs.eu.auth0.com/',
+  authClientId: 'CISn8AsidIlWXUperfhBHUjJ9GhQ3Xuq',
   apiEndpoint: 'https://api.cea.raeffs.dev',
 };
