d19cc5fbdda559dc42090eb3f486668e10469cee
Raphael Fleischlin <git@raeffs.dev>
2020-12-27 20:40:07 +0100

    pipeline

--- frontend/apps/catch-em-all/src/environments/environment.prod.ts
+++ frontend/apps/catch-em-all/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   authIssuer: 'https://raefftec.eu.auth0.com/',
   authClientId: 'w6MvGl002H07JDH86c8DzL6g46sSaeLT',
-  apiEndpoint: 'https://catchemall-api.azurewebsites.net',
+  apiEndpoint: 'https://api.cea.raeffs.dev',
 };
